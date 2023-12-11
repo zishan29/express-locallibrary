003162c4882cbf96b7f6ea638ad64238e5b7f0e5
Zishan Shaikh <100702557+zishan29@users.noreply.github.com>
2023-12-11 10:27:37 +0530

    Add author, Books, genres, book instances pages

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.redirect('/catalog');
 });
 
 module.exports = router;
